56dbf5141f3f1a94288db7e4e4027ea69eea0dbc
AntonWPC <antonsuhodolski@tut.by>
2023-01-04 11:16:22 +0300

    add method replace

--- ClassHW/HWStepic.py
+++ ClassHW/HWStepic.py
@@ -1,15 +1,8 @@
 n = int(input())
 i = 1
 while i < n:
-    if n < 100:
-        if i % 3 == 0 and i % 5 == 0:
-            print(i, end=' ')
-            i += 1
-        else:
-            i += 1
-    else:
-        print("слишком большое значение n")
+    if i ** 2 > n:
+        print(i)
         break
-
-else:
-    pass
+    else:
+        i += 1
