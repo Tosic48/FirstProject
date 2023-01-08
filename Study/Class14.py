5e006a6ccb23e1699bf311c9a1f53241babd68ff
“Anton_Mint” <antonsuhodolski@tut.by>
2023-01-08 11:20:21 +0300

    Work with class

--- Study/Class14.py
+++ Study/Class14.py
@@ -1,2 +1,13 @@
-class Animal:
-    pass
+class Cat:
+    name = 'Bob'
+    color = 'White'
+
+    def say_meow(self):
+        print('Meow!')
+
+
+    def sleep(self):
+        print('Zzzz')
+
+
+Cat()
