a6ce5a77c3642d3c8759f83818daab023c22de96
“Anton_Mint” <antonsuhodolski@tut.by>
2023-01-08 11:46:23 +0300

    Work with class

--- Study/Class14.py
+++ Study/Class14.py
@@ -1,13 +1,27 @@
 class Cat:
     name = 'Bob'
     color = 'White'
+    wild = False
+
+    def __init__(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
 
     def say_meow(self):
-        print('Meow!')
-
+        print(f'{self.name} says "Meow!"')
 
     def sleep(self):
-        print('Zzzz')
+        print(f'{self.name} is slipping: "Zzzz"')
 
 
-Cat()
+    def __str__(self):
+        return f'Cat {self.name}'
+
+    def __repr__(self):
+        return f'Cat {self.name}'
+
+# print(Cat)
+cat1 = Cat('Bob', 'Red', 3)
+cat2 = Cat('Pups', 'White', 4)
+
