1b3e2f46e24f917fa4bae1529c06f8d928264bdc
“Anton_Mint” <antonsuhodolski@tut.by>
2023-01-08 12:42:35 +0300

    Incapsulation

---
+++ Study/Class14_incaps.py
@@ -0,0 +1,29 @@
+class A:
+    a = 10
+    _b = 20
+    __c = 30
+    def __init__(self):
+        self.a1 = 11
+        self._b = 21
+        self.__c1 = 31
+
+
+    def get_attr_a(self):
+        return f'{self.a} {self._b} {self.__c}
+
+
+    def get_attr_b(self):
+        return self._b
+
+
+obj = A()
+
+print(obj.a)
+print(obj.a1)
+print(obj._b)
+print(obj.__dict__)
+print(obj.get_attr_a())
+print(obj.get_attr_b())
+obj.a
+obj._b
+obj.__c
