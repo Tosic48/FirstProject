a4de6c0740bd820b93d5b25293b6e8ed9ef70fbb
AntonWPC <antonsuhodolski@tut.by>
2023-01-09 14:39:35 +0300

    Учим классы ООП

--- ClassHW/HW_Class13_class.py
+++ ClassHW/HW_Class13_class.py
@@ -1,7 +1,13 @@
-#класс
-class Point:
-    "Класс для предоставления координать точек на плостости"
-    # atrubuts
-    color = 'red'
-    circle = 2
+# класс
+class Figure:
+    type_fig= 'ellipse'
+    color= 'red'
 
+
+fig1  = Figure()
+fig1.start_pt= (10, 5)
+fig1.end_pt= (100, 20)
+fig1.color= 'blue'
+
+delattr(fig1, 'color')
+print(*fig1.__dict__)
