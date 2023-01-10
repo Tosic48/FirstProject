d10662b61cd9df3ee6bf8a245359c8f678880f8b
AntonWPC <antonsuhodolski@tut.by>
2023-01-10 16:33:48 +0300

    Классы методы и self

--- ClassHW/HW_Class13_task1.2.py
+++ ClassHW/HW_Class13_task1.2.py
@@ -32,3 +32,18 @@
 #         for i ,key in enumerate(fields):
 #             setattr(self, key,lst_values[i])
 # -------------------------------------------------------
+lst_in = ['1 Сергей 35 120000', '2 Федор 23 12000', '3 Иван 13 1200']
+class DataBase:
+    lst_data = []
+    FIELDS = ('id', 'name', 'old', 'salary')
+
+    def select(self, a, b):
+        return self.lst_data[a: b+1]
+
+    def insert(self, data):
+        for x in data:
+            self.lst_data.append(dict(zip(self.FIELDS, x.split())))
+
+data = DataBase()
+data.insert(lst_in)
+print(data.lst_data)
