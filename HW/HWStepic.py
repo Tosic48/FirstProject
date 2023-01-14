c98902f4e1dc2c5b59679feaf9d8d8c6ad497ae8
“Anton_Mint” <antonsuhodolski@tut.by>
2023-01-14 12:49:13 +0300

    Use peregruz

--- HW/HWStepic.py
+++ HW/HWStepic.py
@@ -1,8 +1,18 @@
-# put your python code here
-# ls = input().split()
-ls = [8, -11, 4, 3, 6]
-sl= []
-for j, i in enumerate(ls):
-    sl.append(i**2)
+things = {'карандаш': 20, 'зеркальце': 100, 'зонт': 500, 'рубашка': 300, 
+          'брюки': 1000, 'бумага': 200, 'молоток': 600, 'пила': 400, 'удочка': 1200,
+          'расческа': 40, 'котелок': 820, 'палатка': 5240, 'брезент': 2130, 'спички': 10}
+sorted_values = sorted(things.values())
+print(sorted_values)
+# weight = int(input()) * 1000
+#
+# answer = []
+# for i in things.values():
+#     print(i)
 
-print(*sl)
+#     if weight - i >= 0:
+#         weight -= i
+#         answer += sort_things[i] + ' '
+#     elif weight - i < 0:
+#         continue
+#
+# print(answer)
