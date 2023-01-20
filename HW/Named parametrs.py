c18dddf08905220c648ba770e3fcde5482e732c6
AntonWPC <antonsuhodolski@tut.by>
2023-01-20 15:09:00 +0300

    именованные parametrs

--- HW/Named parametrs.py
+++ HW/Named parametrs.py
@@ -1,8 +1,20 @@
-def get_v(a,b,c, verbose=True): #verbos формальный параметр
-    if verbose:
-        print(f'a = {a}, b = {b}, c = {c}')
-    return a*b*c
+# def get_v(a,b,c, verbose=True): #verbos формальный параметр
+#     if verbose:
+#         print(f'a = {a}, b = {b}, c = {c}')
+#     return a*b*c
+#
+#
+# v = get_v(1,c=2,b=3) #named in end!!!
+# print(v)
+
+def cmp_str(s1, s2, reg=False, trim=True):
+    if reg:
+        s1 = s1.lower()
+        s2 = s2.lower()
+    if trim:
+        s1 = s1.strip()
+        s2 = s2.strip()
+    return s1 == s2
 
 
-v = get_v(1,c=2,b=3) #named in end!!!
-print(v)
+print(cmp_str('Python ', ' Python', trim=False))
