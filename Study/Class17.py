4c7f6f93784f697bd17d2ea21ad036fbe9cf8ce7
“Anton_Mint” <antonsuhodolski@tut.by>
2023-01-22 11:06:58 +0300

    SQL Study

--- Study/Class17.py
+++ Study/Class17.py
@@ -8,30 +8,36 @@ connect = sqlite3.connect('my_database.db')
 cursor = connect.cursor()
 
 # создание таблицы (если не существует)
-cursor.execute(
-    '''CREATE TABLE IF NOT EXISTS car(id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    model TEXT,
-                    year INT,
-                    color TEXT,
-                    type TEXT)
-    '''
-)
+# cursor.execute(
+#     '''CREATE TABLE IF NOT EXISTS car(id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                     model TEXT,
+#                     year INT,
+#                     color TEXT,
+#                     type TEXT)
+#     '''
+# )
+#
+# # вставка нескольких строк в таблицу
+# cursor.execute(
+#     '''INSERT INTO car(model, year, color, type)
+#        VALUES ('audi', 2010, 'white', 'diesel'),
+#               ('belgily', 2010, 'white', 'diesel'),
+#               ('mtz', 2010, 'white', 'diesel')
+#     '''
+# )
+#
+# # вставка данных с использованием цикла и переменных
+# for i in range(20):
+#     model = random.choice(['audi', 'vw', 'tesla', 'mersedes'])
+#     year = random.randint(2000, 2022)
+#     color = random.choice(['yellow', 'white', 'black', 'red', 'blue'])
+#     type = random.choice(['diesel', 'gas', 'electric'])
+#
+#     cursor.execute('''INSERT INTO car(model, year, color, type)
+#                      VALUES(?,?,?,?)''', (model, year, color, type))
+#
+# # непосредственное выполнение изменений
+# connect.commit()
 
-# вставка нескольких строк в таблицу
-cursor.execute(
-    '''INSERT INTO car(model, year, color, type) 
-       VALUES ('audi', 2010, 'white', 'diesel'),
-              ('belgily', 2010, 'white', 'diesel'),
-              ('mtz', 2010, 'white', 'diesel')
-    '''
-)
-
-# вставка данных с использованием цикла и переменных
-for i in range(20):
-    model = random.choice(['audi', 'vw', 'tesla', 'mersedes'])
-    year = random.randint(2000, 2022)
-    color = random.choice(['yellow', 'white', 'black', 'red', 'blue'])
-    type = random.choice(['diesel', 'gas', 'electric'])
-
-    cursor.execute('''INSERT INTO car(model, year, color, type)
-                     VALUES(?,?,?,?)''', (model, year, color, type))
+cursor.execute('''UPDATE car SET model = "volkswagen" WHERE model = "vw"''')
+connect.commit()
