7d8b70ca5816b027738ed778a445ea5ac2c530d7
“Anton_Mint” <antonsuhodolski@tut.by>
2023-02-04 00:35:42 +0300

    API current time DONE

--- HW/HW API.py
+++ HW/HW API.py
@@ -1,12 +1,21 @@
 import requests
-import pprint   # для вывода информации в более красивом виде
 
-params = {
-    'timeZone': 'Europe/Minsk'
-}
-
-api_result = requests.get('https://www.timeapi.io/api/Time/current/zone', params)
-
-api_response = api_result.json()
-
-pprint.pprint(api_response)
+api_result = requests.get('https://www.timeapi.io')
+print(api_result.status_code)
+if api_result.status_code == 200:
+    while True:
+        city = input('write city of Europe where you live: ')
+        if city:
+            params = {
+                'timeZone': 'Europe/' + city
+            }
+            api_result = requests.get('https://www.timeapi.io/api/Time/current/zone', params)
+            api_response = api_result.json()
+            try:
+                cur_time = api_response["time"]
+                cur_dat = api_response["date"]
+                print(f'current time in {city}: {cur_time} and date: {cur_dat}')
+            except:
+                print('city is not found')
+else:
+    print('Error!')
